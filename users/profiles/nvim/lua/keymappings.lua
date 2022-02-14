cc4095a53134cc5ca1ce4e5e4f8444639e7d7ba9
Varun Narravula <varun@snare.dev>
2022-02-14 01:35:17 -0800

    Add basic notetaking setup
    - Also found a fix for the .envrc after it broke!

--- users/profiles/nvim/lua/keymappings.lua
+++ users/profiles/nvim/lua/keymappings.lua
@@ -1,4 +1,9 @@
 local map = vim.api.nvim_set_keymap
+local bmap = vim.api.nvim_buf_set_keymap
 
 map('t', '<Esc>', '<C-\\><C-n>', {noremap = true}) -- Terminal esc binding
+
 map('n', '<C-n>', ':NvimTreeToggle<CR>', {silent = true, noremap = true}) -- nvim-tree
+
+bmap(0, '', '<Leader>pv', ':! zathura %<.pdf 2>&1 >/dev/null &<CR><CR>', {noremap = true}) -- Open Zathura
+bmap(0, '', '<Leader>pc', ':! pandoc -f markdown -o %<.pdf % 2>&1 >/dev/null<CR>', {noremap = true}) -- Compile pandoc file to pdf
