f5c3a5536c664c6f452ea58221e83adacb63036d
Varun Narravula <varun@snare.dev>
2022-04-19 22:53:37 -0700

    Disable 2FA for GDM (it's cumbersome)

--- profiles/pam/default.nix
+++ profiles/pam/default.nix
@@ -6,13 +6,6 @@
   ];
 
   security.pam.services = {
-    gdm-password.text = lib.mkForce ''
-      auth      substack      login
-      auth      required      ${pkgs.google-authenticator}/lib/security/pam_google_authenticator.so no_increment_hotp nullok
-      account   include       login
-      password  substack      login
-      session   include       login
-    '';
     xscreensaver.text = ''
       auth    sufficient    pam_unix.so try_first_pass likeauth
     '';
