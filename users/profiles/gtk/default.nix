e0e53853ed2c7e9f30a81cdc758ea8e0ac370e16
Varun Narravula <varun@snare.dev>
2022-05-30 00:14:57 -0700

    WIP

--- users/profiles/gtk/default.nix
+++ users/profiles/gtk/default.nix
@@ -1,6 +1,10 @@
 { self, config, pkgs, ... }:
 
 {
+  home.sessionVariables = {
+    GTK_THEME = "Orchis:dark";
+  };
+
   gtk = {
     enable = true;
     font = {
@@ -15,23 +19,5 @@
       name = "Orchis-dark";
       package = pkgs.orchis-theme;
     };
-    gtk3.extraCss = ''
-      decoration
-      {
-          border-radius: 6px 6px 0 0;
-          border-width: 0px;
-          /*box-shadow: 1px 12px 12px 12px rgba(0, 0, 0, 0.4), 0 0 0 1px rgba(0, 0, 0, 0.18);*/
-          box-shadow: none;
-          margin: 4px;
-      }
-      decoration:backdrop
-      {
-          border-radius: 6px 6px 0 0;
-          border-width: 0px;
-          /*box-shadow: 1px 12px 12px 12px rgba(0, 0, 0, 0.4), 0 0 0 1px rgba(0, 0, 0, 0.18);*/
-          box-shadow: none;
-          margin: 4px;
-      }
-    '';
   };
 }
