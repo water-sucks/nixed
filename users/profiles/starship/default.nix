13d84b01517002173370259e414e35d69f28fa91
Varun Narravula <varun@snare.dev>
2022-06-20 15:05:03 -0700

    Add home-manager profiles for command line utils
    - Add profiles for bat, exa, and zoxide
    - Move thefuck and starship into home-manager configurations from
      host-specific configurations
    - Remove stale aliases and initializations from zsh

---
+++ users/profiles/starship/default.nix
@@ -0,0 +1,13 @@
+{ pkgs, lib, ... }:
+
+{
+  programs.starship = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+  };
+
+  home.sessionVariables = {
+    STARSHIP_CONFIG = pkgs.writeText "starship.toml" (lib.fileContents ./starship.toml);
+  };
+}
