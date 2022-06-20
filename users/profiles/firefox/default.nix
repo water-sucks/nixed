aee17af4e3f123f1e16eb5a2936043ccd3db50ed
Varun Narravula <varun@snare.dev>
2022-06-20 15:08:19 -0700

    Add extra platform-specific handling for profiles
    - home-manager profiles now surrounded by lib.mkIf and lib.mkMerge
      when needed for platform-specific options
    - Prune home-manager system defaults and enable extra help/news

--- users/profiles/firefox/default.nix
+++ users/profiles/firefox/default.nix
@@ -1,30 +1,37 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
-{
-  programs.firefox = {
-    enable = true;
-    profiles."default" = {
-      isDefault = true;
-      userContent = builtins.readFile "${pkgs.simplefox-theme}/chrome/userContent.css";
-      userChrome = builtins.readFile "${pkgs.simplefox-theme}/chrome/userChrome.css";
-      extraConfig = builtins.readFile ./user.js;
+let
+  inherit (pkgs.stdenv) isLinux isDarwin;
+in
+lib.mkMerge [
+  {
+    programs.firefox = {
+      enable = true;
+      profiles."default" = {
+        isDefault = true;
+        userContent = builtins.readFile "${pkgs.simplefox-theme}/chrome/userContent.css";
+        userChrome = builtins.readFile "${pkgs.simplefox-theme}/chrome/userChrome.css";
+        extraConfig = builtins.readFile ./user.js;
+      };
     };
-  };
-
-  home.sessionVariables = {
-    MOZ_ENABLE_WAYLAND = "1";
-  };
-
-  xdg.mimeApps.defaultApplications = {
-    "x-scheme-handler/http" = [ "firefox.desktop" ];
-    "x-scheme-handler/https" = [ "firefox.desktop" ];
-    "x-scheme-handler/chrome" = [ "firefox.desktop" ];
-    "text/html" = [ "firefox.desktop" ];
-    "application/x-extension-htm" = [ "firefox.desktop" ];
-    "application/x-extension-html" = [ "firefox.desktop" ];
-    "application/x-extension-shtml" = [ "firefox.desktop" ];
-    "application/xhtml+xml" = [ "firefox.desktop" ];
-    "application/x-extension-xhtml" = [ "firefox.desktop" ];
-    "application/x-extension-xht" = [ "firefox.desktop" ];
-  };
-}
+  }
+  # Doing this separately to avoid Home Manager complaining about package
+  # when default `pkgs.firefox` is explicitly specified
+  (lib.mkIf isDarwin {
+    programs.firefox.package = pkgs.runCommand "firefox-0.0.0" { } "mkdir $out";
+  })
+  (lib.mkIf isLinux {
+    xdg.mimeApps.defaultApplications = {
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "x-scheme-handler/chrome" = [ "firefox.desktop" ];
+      "text/html" = [ "firefox.desktop" ];
+      "application/x-extension-htm" = [ "firefox.desktop" ];
+      "application/x-extension-html" = [ "firefox.desktop" ];
+      "application/x-extension-shtml" = [ "firefox.desktop" ];
+      "application/xhtml+xml" = [ "firefox.desktop" ];
+      "application/x-extension-xhtml" = [ "firefox.desktop" ];
+      "application/x-extension-xht" = [ "firefox.desktop" ];
+    };
+  })
+]
--- users/varun/default.nix
+++ users/varun/default.nix
@@ -1,16 +1,21 @@
-{ self, pkgs, ... }:
+{ self, pkgs, lib, ... }:
 
 {
   users.users.varun = {
     uid = 1000;
     description = "Varun Narravula";
     shell = pkgs.zsh;
-    isNormalUser = true;
+  } // (lib.optionalAttrs pkgs.stdenv.isLinux {
     extraGroups = [ "wheel" "audio" "video" "input" "networkmanager" "libvirtd" "adbusers" "dialout" ];
-  };
+    isNormalUser = true;
+  });
 
-  home-manager.users.varun = { suites, profiles, ... }: {
-    imports = with suites; base ++ dev ++ graphical ++ apps;
+  home-manager.users.varun = { suites, ... }: {
+    imports = with suites;
+      if pkgs.stdenv.isLinux then
+        base ++ dev ++ graphical ++ apps
+      else
+        base ++ dev ++ apps;
 
     programs.git = {
       userEmail = "varun@cvte.org";
