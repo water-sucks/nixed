4bb2520be1ecfb6557a26f37006b995bfb72e9f7
Varun Narravula <varun@cvte.org>
2022-06-21 23:38:34 -0700

    Add nicer kitty icon

--- users/profiles/kitty/default.nix
+++ users/profiles/kitty/default.nix
@@ -1,20 +1,41 @@
-{ self, config, pkgs, ... }:
+{ self, config, pkgs, lib, ... }:
 
-{
-  programs.kitty = {
-    enable = true;
-    package =
-      if pkgs.stdenv.isLinux then
-        pkgs.kitty
-      else
-        pkgs.runCommand "kitty-0.0.0" { } "mkdir $out";
-    font = {
-      name = "BlexMono Nerd Font";
-      package = pkgs.nerdfonts.override { fonts = [ "IBMPlexMono" ]; };
-      size = 10;
-    };
-    extraConfig = ''
-      ${builtins.readFile ./theme.conf}
-    '';
+let
+  inherit (pkgs.stdenv) isLinux isDarwin;
+
+  icon = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/k0nserv/kitty-icon/main/kitty.icns";
+    sha256 = "0n6fb98pj86cm5zl3727vrayxvcbvnkn07sjbyixk4npcmd6w5bg";
   };
-}
+in
+lib.mkMerge [
+  {
+    programs.kitty = {
+      enable = true;
+      package = with pkgs;
+        if isLinux then
+          kitty
+        else
+          runCommand "kitty-0.0.0" { } "mkdir $out";
+      font = {
+        name = "BlexMono Nerd Font";
+        package = pkgs.nerdfonts.override { fonts = [ "IBMPlexMono" ]; };
+        size = 10;
+      };
+      extraConfig = ''
+        ${builtins.readFile ./theme.conf}
+      '';
+    };
+  }
+  (lib.mkIf isDarwin {
+    home.activation.setKittyIcon = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      original_sum=$(sha256sum /Applications/kitty.app/Contents/Resources/kitty.icns)
+
+      /usr/local/bin/fileicon -q set /Applications/kitty.app ${icon}
+
+      if [ "$original_sum" != "$(sha256sum /Applications/kitty.app/Contents/Resources/kitty.icns)" ]; then
+        killall Dock && killall Finder
+      fi
+    '';
+  })
+]
