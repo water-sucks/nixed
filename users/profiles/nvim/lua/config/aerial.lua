fdeb73f965c15894c3b1ccf11c79734368fa612c
Varun Narravula <varun@cvte.org>
2022-07-05 19:58:47 -0700

    Add aerial.nvim code outline plugin

---
+++ users/profiles/nvim/lua/config/aerial.lua
@@ -0,0 +1,23 @@
+require("aerial").setup({
+  backends = { "treesitter", "markdown" }, -- LSP backend disabled, don't need fallback
+  disable_max_lines = 20000,
+  highlight_on_hover = true,
+  default_direction = "left",
+  min_width = 30,
+  on_attach = function(bufnr)
+    require("which-key").register({
+      o = {
+        name = "Outline",
+        t = { ":AerialToggle<CR>", "Toggle outline" },
+        o = { ":AerialOpen<CR>", "Open outline" },
+        c = { ":AerialClose<CR>", "Close outline" },
+      },
+    }, {
+      mode = "n",
+      buffer = bufnr,
+      prefix = "<Leader>",
+      silent = true,
+      noremap = true,
+    })
+  end,
+})
