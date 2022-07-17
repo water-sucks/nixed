a7f92bc1860e93d37f20b19ac9c79eff35c1f0b0
Varun Narravula <varun@cvte.org>
2022-07-16 18:05:30 -0700

    Create SSH configuration
    - Move host SSH configuration into profile
    - Disable annoying askPass prompt that would force a restart of
      display-manager.service
    - Add SSH home-manager config

--- profiles/nixos/pam.nix
+++ profiles/nixos/pam.nix
@@ -1,15 +1,9 @@
 { config, self, pkgs, lib, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    google-authenticator
-  ];
-
   security.pam.services = {
     xscreensaver.text = ''
       auth    sufficient    pam_unix.so try_first_pass likeauth
     '';
-
-    sshd.googleAuthenticator.enable = lib.mkForce true;
   };
 }
