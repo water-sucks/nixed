1f8688598c96bc1e04428ec31bfc8f85200e167b
Varun Narravula <varun@cvte.org>
2022-07-21 01:05:55 -0700

    Add I2P

---
+++ users/profiles/i2p.nix
@@ -0,0 +1,26 @@
+{ self, config, pkgs, lib, ... }:
+
+let
+  inherit (pkgs.stdenv) isLinux;
+
+  sources = pkgs.callPackage firefox/_sources/generated.nix { };
+
+  profile = sources.i2p-profile.src;
+in
+lib.mkMerge [
+  {
+    programs.firefox.profiles."i2p" = {
+      id = 1;
+      isDefault = false;
+      extraConfig = ''
+        ${builtins.readFile "${profile}/src/profile/prefs.js"}
+        ${builtins.readFile "${profile}/src/profile/user.js"}
+      '';
+    };
+  }
+  (lib.mkIf isLinux {
+    home.packages = with pkgs; [
+      i2p
+    ];
+  })
+]
