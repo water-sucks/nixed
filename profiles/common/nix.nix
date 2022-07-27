6d89e66467c76064f7d13bd21b6fe4b0bfc33d76
Varun Narravula <varun@snare.dev>
2022-07-27 03:07:37 -0700

    Update inputs and redo common profile options
    - Separate nix and fontconfig into separate modules; common options that
      will not be repeated are now manually imported from platform-specific modules
    - Make cache config use nix.extraOptions; it's too annoying to use the
      nix module options
    - Remove Digga modules that add caches to prevent extra querying
    - Remove latest channel and unnecessary overrides

--- profiles/common/nix.nix
+++ profiles/common/nix.nix
@@ -1,37 +1,13 @@
-{ self, config, pkgs, lib, ... }:
+{ self, config, ... }:
 
-let
-  inherit (pkgs.stdenv) isLinux isDarwin;
-in
-lib.mkMerge [
-  {
-    nix = {
-      gc.automatic = true;
-      extraOptions = ''
-        min-free = 536870912
-        keep-outputs = true
-        keep-derivations = true
-        fallback = true
-      '';
-    };
-  }
-  (lib.mkIf isDarwin {
-    nix = {
-      useSandbox = true;
-      trustedUsers = [ "@admin" "@wheel" ];
-      allowedUsers = [ "*" ];
-      maxJobs = "auto";
-      buildCores = 0;
-    };
-  })
-  (lib.mkIf isLinux {
-    nix.settings = {
-      sandbox = true;
-      trusted-users = [ "root" "@wheel" ];
-      allowed-users = [ "*" ];
-      max-jobs = "auto";
-      cores = 0;
-      auto-optimise-store = true;
-    };
-  })
-]
+{
+  nix = {
+    gc.automatic = true;
+    extraOptions = ''
+      min-free = 536870912
+      keep-outputs = true
+      keep-derivations = true
+      fallback = true
+    '';
+  };
+}
---
+++ profiles/nixos/fontconfig.nix
@@ -0,0 +1,9 @@
+{ self, config, ... }:
+
+{
+  fonts.fontconfig.defaultFonts = {
+    monospace = [ "BlexMono Nerd Font" ];
+    sansSerif = [ "IBM Plex Sans" "IBM Plex Sans JP" ];
+    serif = [ "IBM Plex Serif" ];
+  };
+}
