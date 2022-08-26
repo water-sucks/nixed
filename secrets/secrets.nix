ce7c37d6cceb0f55d02c6957ec441ed5d9ef18cb
Varun Narravula <varun@snare.dev>
2022-08-26 14:31:38 -0700

    Switch DerekCarr to ZFS filesystem
    - Rekey secrets with new SSH keys
    - Remove outdated mentions/naming of TheBeastNix
    - Replace UUIDs of partitions with ZFS names

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,16 +1,16 @@
 let
-  system-TheBeastNix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINEBrmjWQbaMHbjWB3qz32MePNGfY9hbbTLkYcM2s0td root@TheBeastNix";
-  varun-TheBeastNix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPRQ8/dK+/nLfAhI7yTdrU8sDuSTDoUq3OUFq3vzI8nU varun@TheBeastNix";
-  TheBeastNixKeys = [system-TheBeastNix varun-TheBeastNix];
+  system-DerekCarr = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJTIu+WwsbKCzupRCMVUY/bIpsZUNo2M0uhXutzyHeOb root@DerekCarr";
+  varun-DerekCarr = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG8s+et9SplfHpcjT0+divFZPEjHpmCJ6tIQBYwlBbfB varun@DerekCarr";
+  DerekCarrKeys = [system-DerekCarr varun-DerekCarr];
 
   system-HunterRenfrow = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBhZpIZCXJn6B1I5rixjK0LTlSjMohcYo4v0fxEZjedx root@HunterRenfrow";
   varun-HunterRenfrow = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILSq96xhwCGipQD19sVnrhHMIIqAOGq0jOYdspl/3KdC varun@HunterRenfrow";
   HunterRenfrowKeys = [system-HunterRenfrow varun-HunterRenfrow];
 
-  allKeys = TheBeastNixKeys ++ HunterRenfrowKeys;
+  allKeys = DerekCarrKeys ++ HunterRenfrowKeys;
 in {
-  "varun-user-TheBeastNix.age".publicKeys = TheBeastNixKeys;
-  "root-user-TheBeastNix.age".publicKeys = [system-TheBeastNix];
+  "varun-user-DerekCarr.age".publicKeys = DerekCarrKeys;
+  "root-user-DerekCarr.age".publicKeys = [system-DerekCarr];
 
   "varun-user-HunterRenfrow.age".publicKeys = HunterRenfrowKeys;
   "root-user-HunterRenfrow.age".publicKeys = [system-HunterRenfrow];
