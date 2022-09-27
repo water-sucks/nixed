360933cd7bc649fd8987316d214014b3443fdf7c
Varun Narravula <varun@snare.dev>
2022-09-27 13:16:36 -0700

    Remove flake-compat cruft
    - I use flakes! I USE FLAKES!
    - flake-compat seemed awkward, so I removed it. No one's using it
      anyway, not even myself really. I use `use flake`, but no one
      else uses my configuration that would need flake-compat builtin.

--- lib/compat/nixos/default.nix
+++
@@ -1,9 +0,0 @@
-_: let
-  inherit (default.inputs.nixos) lib;
-
-  host = configs.${hostname} or configs.NixOS;
-  configs = default.nixosConfigurations;
-  default = (import ../.).defaultNix;
-  hostname = lib.fileContents /etc/hostname;
-in
-  host
