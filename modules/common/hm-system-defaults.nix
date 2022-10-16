d2f848221a267fb77708912e0b2866d856f65ad0
Varun Narravula <varun@snare.dev>
2022-10-15 23:50:36 -0700

    Revert "Enable verbosity for home-manager output"
    Yes, I got annoyed on Darwin enough to disable this.

--- modules/common/hm-system-defaults.nix
+++ modules/common/hm-system-defaults.nix
@@ -1,13 +1,10 @@
 {
-  home-manager = {
-    verbose = true;
-    sharedModules = [
-      {
-        programs.home-manager.enable = true;
-        manual.json.enable = true;
-        news.display = "show";
-        home.stateVersion = "21.11";
-      }
-    ];
-  };
+  home-manager.sharedModules = [
+    {
+      programs.home-manager.enable = true;
+      manual.json.enable = true;
+      news.display = "show";
+      home.stateVersion = "21.11";
+    }
+  ];
 }
