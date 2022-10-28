803a071de566d30c52c3fec60e67ffb1264d2753
Varun Narravula <varun@snare.dev>
2022-10-28 10:46:30 -0700

    Add automatic creation for Notes symlink to cloud

--- home/profiles/apps/filen.nix
+++ home/profiles/apps/filen.nix
@@ -1,10 +1,17 @@
 {
+  config,
   pkgs,
   lib,
   ...
 }:
-lib.mkIf pkgs.stdenv.isLinux {
-  home.packages = with pkgs; [
-    filen-desktop
-  ];
-}
+lib.mkMerge [
+  {
+    home.file."Documents/Notes".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Documents/Cloud/Notes";
+  }
+  (lib.mkIf pkgs.stdenv.isLinux {
+    home.packages = with pkgs; [
+      filen-desktop
+    ];
+  })
+]
