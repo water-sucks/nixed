f4a7e995a1db791f49e644e0d15b69ded495cdfb
Varun Narravula <varun@snare.dev>
2022-11-14 15:54:21 -0800

    refactor: build nix-index instead of prefetching
    Prefetching every week can cause inconsistencies in my nixpkgs input
    vs the nix-index DB.

--- home/profiles/base/nix-index.nix
+++ home/profiles/base/nix-index.nix
@@ -5,27 +5,10 @@
 }: let
   inherit (pkgs.stdenv) isLinux isDarwin;
 
-  uname =
-    if isLinux
-    then "${pkgs.coreutils}/bin/uname"
-    else "uname";
-  tr =
-    if isLinux
-    then "${pkgs.coreutils}/bin/tr"
-    else "tr";
-  ln =
-    if isLinux
-    then "${pkgs.coreutils}/bin/ln"
-    else "ln";
-
-  nix-index-update = pkgs.writeShellScriptBin "nix-index-update" ''
-    filename="index-$(${uname} -m)-$(${uname} | ${tr} A-Z a-z)"
+  nix-index-build = pkgs.writeShellScript "build-nix-index.sh" ''
     mkdir -p $HOME/.cache/nix-index
     cd $HOME/.cache/nix-index
-
-    # -N will only download a new version if there is an update.
-    ${pkgs.wget}/bin/wget -q -N https://github.com/Mic92/nix-index-database/releases/latest/download/$filename
-    ${ln} -f $filename files
+    ${pkgs.nix-index}/bin/nix-index
   '';
 in
   lib.mkMerge [
@@ -35,34 +18,30 @@ in
         enableBashIntegration = true;
         enableZshIntegration = true;
       };
-
-      # For if I want to run this manually without
-      # dealing with systemd/launchd shenanigans
-      home.packages = [nix-index-update];
     }
-    # Automatically update nix-index while I'm doing
+    # Automatically build nix-index while I'm doing
     # my Sunday church gigs
     (lib.mkIf isLinux {
-      systemd.user.services.nix-index-update = {
+      systemd.user.services.nix-index-build = {
         Unit = {
-          Description = "Download prebuilt nix-index";
+          Description = "Build nix-index DB";
         };
 
         Service = {
           Type = "oneshot";
-          ExecStart = "${nix-index-update}/bin/nix-index-update";
+          ExecStart = "${nix-index-build}";
         };
       };
 
-      systemd.user.timers.nix-index-update = {
+      systemd.user.timers.nix-index-build = {
         Unit = {
-          Description = "Automatically download prebuilt nix-index";
+          Description = "Automatically build nix-index DB";
         };
 
         Timer = {
           OnCalendar = "Sun *-*-* 08:00:00";
-          Persistent = false; # I don't want it downloading at boot, I may not have internet
-          Unit = "nix-index-update.service";
+          Persistent = false; # I don't want it building at boot, takes way too much time
+          Unit = "nix-index-build.service";
           RandomizedDelaySec = 7200;
         };
 
@@ -70,10 +49,10 @@ in
       };
     })
     (lib.mkIf isDarwin {
-      launchd.agents.nix-index-update = {
+      launchd.agents.nix-index-build = {
         enable = true;
         config = {
-          Program = "${nix-index-update}/bin/nix-index-update";
+          Program = "${nix-index-build}";
           ProcessType = "Background";
 
           StartCalendarInterval = [
