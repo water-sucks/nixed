234512d3b5d2c9f89e1d97a6fb68ad5efb3a14d6
Varun Narravula <varun@cvte.org>
2022-11-18 19:51:26 -0800

    feat: add multiple theme support to LeftWM module
    - Also fix bug with theme not applying - I can't read!

--- home/modules/services/window-managers/leftwm.nix
+++ home/modules/services/window-managers/leftwm.nix
@@ -8,6 +8,28 @@ with lib; let
   cfg = config.xsession.windowManager.leftwm;
 
   ronFormat = pkgs.formats.ron {extensions = ["#![enable(implicit_some)]"];};
+
+  themeType = types.submodule {
+    options = {
+      theme = mkOption {
+        inherit (ronFormat) type;
+        default = {};
+        description = "Theme configuration (not validated)";
+      };
+
+      up = mkOption {
+        type = types.lines;
+        default = "";
+        description = "Theme up script";
+      };
+
+      down = mkOption {
+        type = types.lines;
+        default = "";
+        description = "Theme down script";
+      };
+    };
+  };
 in {
   options.xsession.windowManager.leftwm = {
     enable = mkEnableOption "LeftWM window manager";
@@ -15,58 +37,79 @@ in {
     settings = mkOption {
       inherit (ronFormat) type;
       default = {};
-      description = "Nixified LeftWM configuration (not validated)";
+      description = "LeftWM configuration (not validated)";
     };
 
     theme = mkOption {
-      inherit (ronFormat) type;
+      type = types.str;
+      default = "";
+      description = "Theme to enable; if empty, no current symlink will be created.";
+    };
+
+    themes = mkOption {
+      type = with types; attrsOf (either package themeType);
       default = {};
-      description = "Nixified LeftWM theme configuration (not validated)";
-    };
-
-    up = mkOption {
-      type = types.lines;
-      default = "";
-      description = "Theme-agnostic up script";
-    };
-
-    down = mkOption {
-      type = types.lines;
-      default = "";
-      description = "Theme-agnostic down script";
+      description = "List of themes";
     };
   };
 
   config = mkIf cfg.enable {
-    assertions = with lib; [
-      (hm.assertions.assertPlatform "wayland.windowManager.hikari" pkgs platforms.linux)
-    ];
+    assertions = with lib; let
+      themeNames = attrNames cfg.themes;
+      themePackages = filterAttrs (_: isDerivation) cfg.themes;
+
+      # Check if all theme packages contain a theme.ron
+      # or theme.toml file, up script, and down script
+      packageContainsValidTheme = themeName: theme:
+        all (x: x) [
+          ((builtins.pathExists "${theme}/theme.ron") || (builtins.pathExists "${theme}/theme.toml"))
+          (builtins.pathExists "${theme}/up")
+          (builtins.pathExists "${theme}/down")
+        ];
+    in
+      [
+        (hm.assertions.assertPlatform "xsession.windowManager.leftwm" pkgs platforms.linux)
+
+        # Check if cfg.theme is in the theme list.
+        {
+          assertion = elem cfg.theme themeNames;
+          message = ''
+            ${cfg.theme} is not in the list of themes.
+            Valid themes are:
+              * ${concatStringsSep "\n  * " themeNames}
+          '';
+        }
+      ]
+      ++ (mapAttrsToList (n: v: {
+          assertion = packageContainsValidTheme n v;
+          message = "${n} is not a valid theme.";
+        })
+        themePackages);
 
     home.packages = [pkgs.leftwm];
 
-    xdg.configFile = {
-      "leftwm/config.ron" = mkIf (cfg.settings != {}) {
-        source = ronFormat.generate "leftwm-config" cfg.settings;
-      };
-      "leftwm/themes/current/theme.ron" = mkIf (cfg.settings != {}) {
-        source = ronFormat.generate "leftwm-theme" cfg.theme;
-      };
-
-      # Manually written so that shebang can pass through
-      "leftwm/themes/current/up" = mkIf (cfg.up != "") {
-        source = pkgs.writeTextFile {
-          name = "up.sh";
-          text = cfg.up;
-          executable = true;
+    xdg.configFile = mkMerge (flatten [
+      {
+        "leftwm/config.ron" = mkIf (cfg.settings != {}) {
+          source = ronFormat.generate "config.ron" cfg.settings;
         };
-      };
-      "leftwm/themes/current/down" = mkIf (cfg.down != "") {
-        source = pkgs.writeTextFile {
-          name = "down.sh";
-          text = cfg.down;
-          executable = true;
+        "leftwm/themes/current" = mkIf (cfg.theme != "") {
+          source =
+            config.lib.file.mkOutOfStoreSymlink
+            "${config.home.homeDirectory}/.config/leftwm/themes/${cfg.theme}";
         };
-      };
-    };
+      }
+      (mapAttrsToList (n: v:
+        if isDerivation v
+        then {
+          "leftwm/themes/${n}".source = v;
+        }
+        else {
+          "leftwm/themes/${n}/theme.ron".source = ronFormat.generate "theme.ron" v.theme;
+          "leftwm/themes/${n}/up".source = pkgs.writeShellScript "up" v.up;
+          "leftwm/themes/${n}/down".source = pkgs.writeShellScript "down" v.down;
+        })
+      cfg.themes)
+    ]);
   };
 }
