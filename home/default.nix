9365bbf131ccd0104a1be36081735fcafe9ee35c
Varun Narravula <varun@snare.dev>
2022-11-24 14:47:19 -0800

    refactor: make importModules function and organize

--- home/default.nix
+++ home/default.nix
@@ -1,17 +1,5 @@
-{
-  self,
-  lib,
-  ...
-}:
-with lib; let
-  inherit (self.lib) flattenTree rakeLeaves;
-
-  moduleName = name: let
-    path = splitString "." name;
-  in
-    last path;
-in {
+{self, ...}: {
   flake = {
-    homeModules = mapAttrs' (n: nameValuePair (moduleName n)) (flattenTree (rakeLeaves ./modules));
+    homeModules = self.lib.importModules ./modules;
   };
 }
