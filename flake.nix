df99b838a2243886b868428bf8da986eb084c61a
Varun Narravula <varun@snare.dev>
2022-12-01 17:26:47 -0800

    feat: upgrade stable nixpkgs input to 22.11

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/release-22.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/release-22.11";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
 
