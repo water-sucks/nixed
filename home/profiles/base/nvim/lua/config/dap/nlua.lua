0863954f4d3458a92b8e6076c7d7066a13a6c9b6
Varun Narravula <varun@snare.dev>
2022-12-20 16:23:11 -0800

    fix: add better defaults for lua debugging

--- home/profiles/base/nvim/lua/config/dap/nlua.lua
+++ home/profiles/base/nvim/lua/config/dap/nlua.lua
@@ -6,24 +6,28 @@ dap.configurations.lua = {
     request = "attach",
     name = "Attach to running Neovim instance",
     host = function()
-      local value = vim.fn.input("Host [127.0.0.1]: ")
+      local value = vim.fn.input({ prompt = "Host [127.0.0.1]: ", default = "127.0.0.1" })
       if value ~= "" then
         return value
       end
       return "127.0.0.1"
     end,
     port = function()
-      local value = tonumber(vim.fn.input("Port: "))
+      local value = tonumber(vim.fn.input({ prompt = "Port: ", default = "9263" }))
       assert(value, "Please provide a port number")
       return value
     end,
   },
 }
 
+vim.api.nvim_create_user_command("OSVStart", function()
+  require("osv").launch({ port = 9263 })
+end, {})
+
 dap.adapters.nlua = function(callback, config)
   callback({
     type = "server",
-    host = config.host,
-    port = config.port,
+    host = config.host or "127.0.0.1",
+    port = config.port or "9263",
   })
 end
