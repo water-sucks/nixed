5c34044f3e222f77cb5268cfc5b21185d0582245
Varun Narravula <varun@snare.dev>
2022-12-20 16:36:29 -0800

    feat: add search result count to lualine

--- home/profiles/base/nvim/lua/config/lualine.lua
+++ home/profiles/base/nvim/lua/config/lualine.lua
@@ -138,6 +138,20 @@ left({
   },
 })
 
+right({
+  function()
+    if vim.api.nvim_get_vvar("hlsearch") == 1 then
+      local res = vim.fn.searchcount({ maxcount = 999, timeout = 500 })
+
+      if res.total > 0 then
+        return string.format("%d/%d", res.current, res.total)
+      end
+    end
+
+    return ""
+  end,
+})
+
 right({ "filetype" })
 
 right({
