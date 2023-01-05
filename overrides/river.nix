a2175de17c7d6595ea68e5216ac71011334a6305
Varun Narravula <varun@snare.dev>
2023-01-05 15:48:59 -0800

    fix: remove outdated wlroots 0.16 override for river

--- overrides/river.nix
+++ overrides/river.nix
@@ -1,28 +1,24 @@
 _final: prev: {
-  river =
-    (prev.river.overrideAttrs (_: {
-      patches = [./patches/change-default-color.patch];
+  river = prev.river.overrideAttrs (_: {
+    patches = [./patches/change-default-color.patch];
 
-      postFixup = let
-        launcher = prev.writeShellScript "river-launcher.sh" ''
-          . "/etc/profiles/per-user/$USER/etc/profile.d/hm-session-vars.sh"
-          sleep 1
-          # systemctl --user start graphical-session.target
-          systemd-cat --identifier=river river
-        '';
-      in ''
-        mkdir -p $out/share/wayland-sessions
-        cp contrib/river.desktop $out/share/wayland-sessions
-
-        substituteInPlace $out/share/wayland-sessions/river.desktop \
-          --replace "Exec=river" "Exec=${launcher}"
+    postFixup = let
+      launcher = prev.writeShellScript "river-launcher.sh" ''
+        . "/etc/profiles/per-user/$USER/etc/profile.d/hm-session-vars.sh"
+        sleep 1
+        # systemctl --user start graphical-session.target
+        systemd-cat --identifier=river river
       '';
+    in ''
+      mkdir -p $out/share/wayland-sessions
+      cp contrib/river.desktop $out/share/wayland-sessions
 
-      passthru.providedSessions = ["river"];
+      substituteInPlace $out/share/wayland-sessions/river.desktop \
+        --replace "Exec=river" "Exec=${launcher}"
+    '';
 
-      inherit (prev.sources.river) pname version src;
-    }))
-    .override {
-      wlroots = prev.wlroots_0_16;
-    };
+    passthru.providedSessions = ["river"];
+
+    inherit (prev.sources.river) pname version src;
+  });
 }
