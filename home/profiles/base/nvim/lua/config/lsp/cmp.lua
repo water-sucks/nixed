f0518d87e1352575a195e1b2804d088d35d3d744
Varun Narravula <varun@snare.dev>
2023-01-09 16:19:14 -0800

    fix: only complete on explicitly selected items
    Why didn't I do this before?
    
    I have brain damage.

--- home/profiles/base/nvim/lua/config/lsp/cmp.lua
+++ home/profiles/base/nvim/lua/config/lsp/cmp.lua
@@ -39,7 +39,7 @@ cmp.setup({
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.abort(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<CR>"] = cmp.mapping.confirm({ select = false }),
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
