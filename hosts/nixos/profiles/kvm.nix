03ea6f94386c58e5a187b8ebc8290948775684a9
Varun Narravula <varun@snare.dev>
2023-02-24 14:11:44 -0800

    fix: remove need for xattr permissions for KVM

--- hosts/nixos/profiles/kvm.nix
+++ hosts/nixos/profiles/kvm.nix
@@ -1,5 +1,14 @@
 {pkgs, ...}: {
-  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      runAsRoot = true;
+      verbatimConfig = ''
+        remember_owner=0
+        namespaces=[]
+      '';
+    };
+  };
 
   environment.systemPackages = with pkgs; [
     virt-manager
