88098a3b30cde4fdd45eceb2c796e004cfa552ac
Varun Narravula <varun@snare.dev>
2023-03-16 18:00:57 -0700

    fix: use stable python for working debugpy
    This will be reverted once debugpy is working in nixpkgs again.

--- home/profiles/dev/python.nix
+++ home/profiles/dev/python.nix
@@ -2,7 +2,7 @@
   # Sadly, I need this in my global environment.
   # So let's make a profile for it! WCGW, amirite?
   home.packages = with pkgs; [
-    (python3.withPackages (p: [
+    (stable.python3.withPackages (p: [
       p.debugpy
     ]))
   ];
