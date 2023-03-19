82d92241c09e2cc9105b2a9cfc2182c75974153c
Varun Narravula <varun@snare.dev>
2023-03-19 16:11:37 -0700

    fix: re-enable nitropy module
    Overriding spsdk because it did not have a couple of modules required to
    build, but this is just a minor inconvenience. This will be resolved by
    a later update.

--- hosts/nixos/profiles/nitrokey.nix
+++ hosts/nixos/profiles/nitrokey.nix
@@ -1,7 +1,27 @@
 {pkgs, ...}: {
   hardware.nitrokey.enable = true;
 
-  # environment.systemPackages = with pkgs; [
-  #   pynitrokey
-  # ];
+  environment.systemPackages = let
+    pynitrokey = pkgs.pynitrokey.override {
+      python3Packages = pkgs.python3Packages.override {
+        overrides = _self: super: {
+          spsdk = super.spsdk.overridePythonAttrs (o: {
+            propagatedBuildInputs =
+              o.propagatedBuildInputs
+              ++ [
+                super.typing-extensions
+              ];
+            pythonRelaxDeps =
+              o.pythonRelaxDeps
+              ++ [
+                "typing-extensions"
+                "pycryptodome"
+              ];
+          });
+        };
+      };
+    };
+  in [
+    pynitrokey
+  ];
 }
