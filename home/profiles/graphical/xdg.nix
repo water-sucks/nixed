8a72fb841700931e1b0fc7f55c04918776570668
Varun Narravula <varun@snare.dev>
2023-03-31 14:52:58 -0700

    fix: forcibly delete mimeapps.list on activation
    Certain applications tend to remove the file and write to it (I'm
    looking at you, Unity games), so activation and backup both fail if
    there is already an existing file. Since mimeapps.list is supposed
    to be immutable, it's okay to remove the link during activation.

--- home/profiles/graphical/xdg.nix
+++ home/profiles/graphical/xdg.nix
@@ -5,4 +5,10 @@
 }:
 lib.mkIf pkgs.stdenv.isLinux {
   xdg.mimeApps.enable = true;
+
+  home.activation = {
+    deleteMimeappsList = lib.hm.dag.entryBefore ["checkLinkTargets"] ''
+      rm $VERBOSE_ARG $HOME/.config/mimeapps.list
+    '';
+  };
 }
