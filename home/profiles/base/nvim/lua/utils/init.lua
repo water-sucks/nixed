7a14075865887cce47956904e6acd6047628d001
Varun Narravula <varun@snare.dev>
2023-04-27 15:24:25 -0700

    fix: work around lazy.nvim dev plugin error
    lazy.nvim does not fall back to the Nix-generated version of the
    plugin, so I added extra handling logic to the `use` function to
    use it instead of lazy's.

--- home/profiles/base/nvim/lua/utils/init.lua
+++ home/profiles/base/nvim/lua/utils/init.lua
@@ -4,16 +4,22 @@
 _G.use = function(name, spec)
   spec = spec or {}
 
-  if spec.dev then
-    spec[1] = name
-    return spec
-  end
-
   local plugin_name = name:match("[^/]+$")
   local plugin_dir = vim.fn.stdpath("data") .. "/plugins/" .. plugin_name
 
+  -- This works around the automatic dev plugin functionality, and falls
+  -- back properly to the Nix-generated version if it exists.
+  if spec.dev then
+    local dev_plugin_dir = vim.env.HOME .. "/Code/NeovimPlugins/" .. plugin_name
+    if vim.fn.isdirectory(dev_plugin_dir) > 0 then
+      spec.dir = dev_plugin_dir
+      spec.dev = nil
+      return spec
+    end
+  end
+
   if vim.fn.isdirectory(plugin_dir) > 0 then
-    spec["dir"] = plugin_dir
+    spec.dir = plugin_dir
   else
     spec[1] = name
   end
