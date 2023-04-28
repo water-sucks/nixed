973efb33c13f07b47ea19be071b3728fa3b16c89
Autumn Wind <autumnwind@snare.dev>
2023-04-28 12:29:13 +0000

    chore: update flake inputs
    - Remove tidal-hifi name/icon patching

--- home/profiles/apps/music.nix
+++ home/profiles/apps/music.nix
@@ -11,14 +11,7 @@
         --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath o.buildInputs}" \
         "''${gappsWrapperArgs[@]}"
       substituteInPlace $out/share/applications/tidal-hifi.desktop \
-        --replace "Exec=/opt/tidal-hifi/tidal-hifi" "Exec=$out/bin/tidal-hifi" \
-        --replace "Name=tidal-hifi" "Name=TIDAL"
-
-      for size in 48 64 128 256 512; do
-        mkdir -p $out/share/icons/hicolor/''${size}x''${size}/apps/
-        convert $out/share/icons/hicolor/0x0/apps/tidal-hifi.png \
-          -resize ''${size}x''${size} $out/share/icons/hicolor/''${size}x''${size}/apps/icon.png
-      done
+        --replace "Exec=/opt/tidal-hifi/tidal-hifi" "Exec=$out/bin/tidal-hifi"
     '';
   });
 in
