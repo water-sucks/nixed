0698f913f4081383e22902796e88fef1c2d9852f
Varun Narravula <varun@snare.dev>
2023-05-12 02:05:32 -0700

    fix: wrap armcord with pristine xdg-open instance
    The xdg-open provided by default has an LD_LIBRARY_PATH that didn't
    let ArmCord open links, so I had to unset it in a wrapper.

--- home/profiles/apps/armcord.nix
+++ home/profiles/apps/armcord.nix
@@ -3,26 +3,45 @@
   pkgs,
   lib,
   ...
-}:
-lib.mkIf pkgs.stdenv.isLinux {
-  home.packages = with pkgs; [
-    armcord
-  ];
-
-  home.persistence."/persist/home/${config.home.username}" = {
-    directories = [
-      ".config/ArmCord"
+}: let
+  armcord = let
+    pristineXdgOpen =
+      pkgs.runCommandWith {
+        name = "pristine-xdg-open";
+        derivationArgs.nativeBuildInputs = [pkgs.makeWrapper];
+      } ''
+        # Wrap xdg-open, unsetting `LD_LIBRARY_PATH` becuase it's used by
+        # the `armcord` package to inject libraries at runtime; these conflict
+        # with Firefox, etc.
+        makeWrapper ${pkgs.xdg-utils}/bin/xdg-open $out/bin/xdg-open \
+          --unset LD_LIBRARY_PATH
+      '';
+  in
+    pkgs.armcord.overrideAttrs (_: {
+      postInstall = ''
+        wrapProgram $out/bin/armcord --prefix PATH : "${pristineXdgOpen}/bin"
+      '';
+    });
+in
+  lib.mkIf pkgs.stdenv.isLinux {
+    home.packages = [
+      armcord
     ];
-  };
 
-  xdg.configFile = {
-    "ArmCord/themes/DarkDiscord/DarkDiscord.css".text = ''
-      @import url(https://discord-modifications.github.io/dark-discord/src/source.css);
-    '';
-    "ArmCord/themes/DarkDiscord/manifest.json".text = builtins.toJSON {
-      name = "DarkDiscord";
-      author = "discord-modifications";
-      theme = "DarkDiscord.css";
+    home.persistence."/persist/home/${config.home.username}" = {
+      directories = [
+        ".config/ArmCord"
+      ];
     };
-  };
-}
+
+    xdg.configFile = {
+      "ArmCord/themes/DarkDiscord/DarkDiscord.css".text = ''
+        @import url(https://discord-modifications.github.io/dark-discord/src/source.css);
+      '';
+      "ArmCord/themes/DarkDiscord/manifest.json".text = builtins.toJSON {
+        name = "DarkDiscord";
+        author = "discord-modifications";
+        theme = "DarkDiscord.css";
+      };
+    };
+  }
