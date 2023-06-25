be0d11e236e8153b128718f9687f00e6c4c372f9
Varun Narravula <varun@snare.dev>
2023-06-24 20:00:25 -0700

    fix: use 15 minute interval for xss-lock
    swayidle is configured to wait for 15 minutes so why shouldn't
    xss-lock?

--- home/profiles/graphical/x11/xsecurelock.nix
+++ home/profiles/graphical/x11/xsecurelock.nix
@@ -8,7 +8,7 @@
   services.screen-locker = {
     enable = true;
     lockCmd = "/usr/bin/env XSECURELOCK_SAVER=saver_blank XSECURELOCK_COMPOSITE_OBSCURER=0 XSECURELOCK_PAM_SERVICE=xscreensaver XSECURELOCK_PASSWORD_PROMPT=asterisks ${pkgs.xsecurelock}/bin/xsecurelock";
-    inactiveInterval = 5;
+    inactiveInterval = 15;
     xss-lock.package = pkgs.xss-lock;
     xss-lock.extraOptions = [
       "-n ${pkgs.xsecurelock}/libexec/xsecurelock/dimmer"
