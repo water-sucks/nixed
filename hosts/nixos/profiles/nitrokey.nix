e9f689b6d1c8340c8d76645078a5863c0d38cecf
Varun Narravula <varun@snare.dev>
2023-08-15 11:27:08 -0700

    fix: resolve pynitrokey breakage yet again

--- hosts/nixos/profiles/nitrokey.nix
+++ hosts/nixos/profiles/nitrokey.nix
@@ -1,7 +1,44 @@
 {pkgs, ...}: {
   hardware.nitrokey.enable = true;
 
-  environment.systemPackages = [
-    pkgs.pynitrokey
+  environment.systemPackages = let
+    python3Packages = pkgs.python3Packages.override {
+      overrides = _final: prev: {
+        spsdk = prev.spsdk.overridePythonAttrs (o: rec {
+          version = "1.11.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "nxp-mcuxpresso";
+            repo = o.pname;
+            rev = "refs/tags/${version}";
+            hash = "sha256-B3qedAXSG3A8rcWu1O2GnZ1ZqHN+7fQK43qXzGnDEY0=";
+          };
+
+          nativeCheckInputs =
+            o.nativeCheckInputs
+            ++ (with prev; [
+              importlib-metadata
+              pyftdi
+            ]);
+
+          pythonRelaxDeps =
+            o.pythonRelaxDeps
+            ++ [
+              # Leave this here, because it is common to relax
+              # deps for this package.
+              "cryptography"
+            ];
+        });
+      };
+    };
+
+    pynitrokey =
+      (pkgs.pynitrokey.override {
+        inherit python3Packages;
+      })
+      .overridePythonAttrs (o: {
+        pythonRelaxDeps = o.pythonRelaxDeps ++ ["click"];
+      });
+  in [
+    pynitrokey
   ];
 }
