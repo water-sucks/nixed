53d3e62440ecb4381f79bcc7d5c58f9ecf91bec3
Varun Narravula <varun@snare.dev>
2023-08-16 11:37:40 -0700

    fix: rename fonts.fonts to fonts.packages
    The former option has been deprecated, so this is an ugly workaround
    to remove the deprecation message on NixOS but keep the same name on
    macOS.

--- hosts/profiles/fonts.nix
+++ hosts/profiles/fonts.nix
@@ -1,6 +1,12 @@
 {pkgs, ...}: {
-  fonts = {
-    fonts = with pkgs; [
+  fonts = let
+    packages-key = "${
+      if pkgs.stdenv.isDarwin
+      then "fonts"
+      else "packages"
+    }";
+  in {
+    ${packages-key} = with pkgs; [
       ibm-plex
       (nerdfonts.override {fonts = ["IBMPlexMono"];})
       font-awesome
