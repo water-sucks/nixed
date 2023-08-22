afbf16226d415cfb429dc12372a4b4f3d1ac086e
Autumn Wind <autumnwind@snare.dev>
2023-08-22 12:33:28 +0000

    chore: update flake inputs

--- home/profiles/dev/python.nix
+++ home/profiles/dev/python.nix
@@ -3,7 +3,13 @@
   # So let's make a profile for it! WCGW, amirite?
   home.packages = [
     (pkgs.python3.withPackages (p: [
-      p.debugpy
+      (p.debugpy.overridePythonAttrs (o: {
+        disabledTests =
+          o.disabledTests
+          ++ [
+            "test_attach_pid_client"
+          ];
+      }))
     ]))
   ];
 }
