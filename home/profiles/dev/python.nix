3bf8cf4158bc905897ce5206faa28560c0d620f9
Varun Narravula <varun@snare.dev>
2023-09-21 21:27:16 -0700

    chore: un-disable debugpy test
    So that it can be fetched from the cache instead of rebuilding.

--- home/profiles/dev/python.nix
+++ home/profiles/dev/python.nix
@@ -3,13 +3,7 @@
   # So let's make a profile for it! WCGW, amirite?
   home.packages = [
     (pkgs.python3.withPackages (p: [
-      (p.debugpy.overridePythonAttrs (o: {
-        disabledTests =
-          o.disabledTests
-          ++ [
-            "test_attach_pid_client"
-          ];
-      }))
+      p.debugpy
     ]))
   ];
 }
