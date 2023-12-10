ec7461510e33ab4b17f8523375a94f33c3c65c9f
Varun Narravula <varun@snare.dev>
2023-12-09 22:20:13 -0800

    chore: remove outdated nitropy overrides
    Fucking finally.

--- hosts/nixos/profiles/nitrokey.nix
+++ hosts/nixos/profiles/nitrokey.nix
@@ -1,47 +1,7 @@
 {pkgs, ...}: {
   hardware.nitrokey.enable = true;
 
-  environment.systemPackages = let
-    python3Packages = pkgs.python3Packages.override {
-      overrides = _final: prev: {
-        spsdk =
-          (prev.spsdk.override {
-            pyocd = prev.pyocd.overridePythonAttrs (o: {
-              propagatedBuildInputs = o.propagatedBuildInputs ++ (with prev; [importlib-metadata importlib-resources lark]);
-            });
-          })
-          .overridePythonAttrs (o: {
-            nativeCheckInputs =
-              o.nativeCheckInputs
-              ++ (with prev; [
-                importlib-metadata
-                pyftdi
-              ]);
-
-            pythonRelaxDeps =
-              o.pythonRelaxDeps
-              ++ [
-                # Leave this here, because it is common to relax
-                # deps for this package.
-                "cryptography"
-              ];
-          });
-      };
-    };
-
-    pynitrokey =
-      (pkgs.pynitrokey.override {
-        inherit python3Packages;
-      })
-      .overridePythonAttrs (o: {
-        pythonRelaxDeps = o.pythonRelaxDeps ++ ["click"];
-        propagatedBuildInputs =
-          o.propagatedBuildInputs
-          ++ [
-            python3Packages.importlib-metadata
-          ];
-      });
-  in [
-    pynitrokey
+  environment.systemPackages = [
+    pkgs.pynitrokey
   ];
 }
