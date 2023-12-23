a0eaa0a39ac72395421b3069f68082b4bdd06b6f
Varun Narravula <varun@snare.dev>
2023-12-23 00:45:51 -0800

    feat: add fzf

---
+++ home/profiles/dev/fzf.nix
@@ -0,0 +1,29 @@
+{config, ...}: let
+  c = config.colorscheme.colors;
+in {
+  programs.fzf = {
+    enable = true;
+    defaultOptions = [
+      "--height 69%"
+      "--border"
+    ];
+    defaultCommand = "fd --type f";
+    fileWidgetCommand = "fd --type f";
+    colors = {
+      fg = "#${c.dark-pink}";
+      bg = "#${c.bg}";
+      hl = "#${c.dark-red}";
+      "fg+" = "#${c.pink}";
+      "bg+" = "#${c.dark-purple}";
+      gutter = "#${c.dark-purple}";
+      "hl+" = "#${c.red}";
+      info = "#${c.magenta}";
+      border = "#${c.dark-purple}";
+      prompt = "#${c.fg-secondary}";
+      pointer = "#${c.dark-red}";
+      marker = "#${c.dark-red}";
+      spinner = "#${c.gray}";
+      header = "#${c.gray}";
+    };
+  };
+}
