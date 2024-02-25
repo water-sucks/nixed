e7b67c1fa504543fba229b3d7b039d6f2e5808f7
Varun Narravula <varun@snare.dev>
2024-02-24 19:01:05 -0800

    refactor: override desktop file for tidal-hifi
    Now tidal-hifi is fetched from the cache instead of being built on
    this machine. Yay!

--- home/profiles/apps/music.nix
+++ home/profiles/apps/music.nix
@@ -3,30 +3,26 @@
   pkgs,
   lib,
   ...
-}: let
-  tidal-hifi = pkgs.tidal-hifi.overrideAttrs (o: {
-    postFixup = ''
-      makeWrapper $out/opt/tidal-hifi/tidal-hifi $out/bin/tidal-hifi \
-        --add-flags "--no-sandbox" \
-        --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath o.buildInputs}" \
-        "''${gappsWrapperArgs[@]}"
-      substituteInPlace $out/share/applications/tidal-hifi.desktop \
-        --replace "Exec=/opt/tidal-hifi/tidal-hifi" "Exec=$out/bin/tidal-hifi"
-    '';
-  });
-in
-  lib.mkIf pkgs.stdenv.isLinux {
-    home.packages = [
-      tidal-hifi
+}:
+lib.mkIf pkgs.stdenv.isLinux {
+  home.packages = [
+    pkgs.tidal-hifi
+  ];
+
+  xdg.desktopEntries.tidal-hifi = {
+    name = "TIDAL Hi-Fi";
+    exec = "tidal-hifi --no-sandbox";
+    icon = "tidal-hifi";
+    terminal = false;
+  };
+
+  home.persistence.${config.persistence.directory} = {
+    directories = [
+      ".config/tidal-hifi"
     ];
+  };
 
-    home.persistence.${config.persistence.directory} = {
-      directories = [
-        ".config/tidal-hifi"
-      ];
-    };
-
-    xdg.mimeApps.defaultApplications = {
-      "x-scheme-handler/tidal" = ["tidal-hifi.desktop"];
-    };
-  }
+  xdg.mimeApps.defaultApplications = {
+    "x-scheme-handler/tidal" = ["tidal-hifi.desktop"];
+  };
+}
