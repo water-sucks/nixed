e32d63d0ed89a6d269cac9bb65d97ed14799fcff
Varun Narravula <varun@snare.dev>
2024-03-06 01:13:49 -0800

    feat: add nitropy get-otp+list aliases

--- home/profiles/base/nitrokey.nix
+++ home/profiles/base/nitrokey.nix
@@ -2,4 +2,19 @@
   home.packages = [
     pkgs.stable.pynitrokey
   ];
+
+  programs.zsh.initExtra = ''
+    function nkso() {
+      otp="$(nitropy nk3 secrets get-otp "$1" | tail -1 | xargs printf "%s")"
+      if [ "$WAYLAND_DISPLAY" != "" ]; then
+        wl-copy "$otp"
+      else
+        echo "$otp" | xclip
+      fi
+    }
+
+    function nksol() {
+      nitropy nk3 secrets list | awk '{print $2}'
+    }
+  '';
 }
