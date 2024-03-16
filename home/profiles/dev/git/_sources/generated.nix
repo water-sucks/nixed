75951b604a50d1c147a82c476be8de732313900f
Autumn Wind <autumnwind@snare.dev>
2024-03-16 12:20:02 +0000

    chore: update nvfetcher sources

--- home/profiles/dev/git/_sources/generated.nix
+++ home/profiles/dev/git/_sources/generated.nix
@@ -7,14 +7,14 @@
 }: {
   git-aliases = {
     pname = "git-aliases";
-    version = "12c40fc70b37c9a26dd9f8ff72f61ca7d98e32d2";
+    version = "7b941c3abbcee391b6bfc4f8d6b8372064245b49";
     src = fetchFromGitHub {
       owner = "GitAlias";
       repo = "gitalias";
-      rev = "12c40fc70b37c9a26dd9f8ff72f61ca7d98e32d2";
+      rev = "7b941c3abbcee391b6bfc4f8d6b8372064245b49";
       fetchSubmodules = false;
-      sha256 = "sha256-At11Q9zmtM5mj6nD5oA1656AUWCMxt6U5FU8pbOIeM4=";
+      sha256 = "sha256-IvHM6mRtoeVm01cUmTkKqjm6/n3Izau89B7MT69Afo0=";
     };
-    date = "2023-10-22";
+    date = "2024-03-15";
   };
 }
