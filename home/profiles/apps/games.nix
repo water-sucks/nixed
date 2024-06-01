f15ecaff722781a78fc2a51e9c363246e46ac8d4
Varun Narravula <varun@snare.dev>
2024-06-01 13:35:50 -0700

    feat(games): add dolphin emulator

--- home/profiles/apps/games.nix
+++ home/profiles/apps/games.nix
@@ -8,12 +8,14 @@ lib.mkIf pkgs.stdenv.isLinux {
   home.packages = with pkgs; [
     nsmb-mvl
     vics-mvlo
+    dolphin-emu
   ];
 
   home.persistence.${config.persistence.directory} = {
     directories = [
       ".config/unity3d/ipodtouch0218/NSMB-MarioVsLuigi"
       ".config/unity3d/vlco_o/NSMBVersus_ vic's Custom Match-inator"
+      ".config/dolphin-emu"
       {
         directory = ".steam";
         method = "symlink";
