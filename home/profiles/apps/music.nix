d0d04b63409fe738985ed688acb56d373b1f23d9
Varun Narravula <varun@snare.dev>
2024-06-02 18:18:21 -0700

    fix(tidal): manually override version to 5.13.1
    I will change this once the update is merged into master.

--- home/profiles/apps/music.nix
+++ home/profiles/apps/music.nix
@@ -4,7 +4,12 @@
   lib,
   ...
 }: let
-  tidal-hifi = pkgs.tidal-hifi.overrideAttrs (o: {
+  tidal-hifi = pkgs.tidal-hifi.overrideAttrs (o: rec {
+    version = "5.13.1";
+    src = pkgs.fetchurl {
+      url = "https://github.com/Mastermindzh/tidal-hifi/releases/download/${version}/tidal-hifi_${version}_amd64.deb";
+      sha256 = "sha256-TnvVm0qjm41if90+f9YtO8wR6NYg6oUZbZd0RiUU33M=";
+    };
     buildInputs =
       o.buildInputs
       ++ [
