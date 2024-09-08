51321fc3bf21a0c6b0b12a7720a2b4115095d3ac
Varun Narravula <varun@snare.dev>
2024-09-08 10:30:38 -0700

    fix: remove armcord overrides

--- home/profiles/apps/armcord.nix
+++ home/profiles/apps/armcord.nix
@@ -5,47 +5,16 @@
   ...
 }: let
   inherit (pkgs.stdenv) isLinux isDarwin;
-
-  armcord = pkgs.armcord.overrideAttrs (o: rec {
-    version = "3.2.8";
-    src = pkgs.fetchFromGitHub {
-      owner = "ArmCord";
-      repo = "ArmCord";
-      rev = "v${version}";
-      hash = "sha256-H/Y3xA7gE24UsUkrxmrRFSvs16qZCVxli9vdnt7ihi8=";
-    };
-    pnpmDeps = pkgs.pnpm.fetchDeps {
-      inherit (o) pname;
-      inherit version src;
-      hash = "sha256-hYp1XbWQL5NbIzzUSnZ7y7V+vYQmymRNo+EiSjn5d9E=";
-    };
-    desktopItems = [
-      (pkgs.makeDesktopItem {
-        name = "armcord";
-        desktopName = "ArmCord";
-        exec = "armcord %U";
-        icon = "armcord";
-        comment = o.meta.description;
-        categories = ["Network"];
-        startupWMClass = "ArmCord";
-        terminal = false;
-      })
-    ];
-  });
 in
   lib.mkMerge [
     (lib.mkIf isLinux {
       home.packages = [
-        armcord
+        pkgs.armcord
       ];
 
       home.persistence.${config.persistence.directory} = {
         directories = [
-          # This is a little strange, but we're keeping both in case
-          # the code gets any weirder when dealing with case-insensitive
-          # file systems
           ".config/armcord"
-          ".config/ArmCord"
         ];
       };
     })
@@ -53,8 +22,8 @@ in
       home.file = let
         target =
           if isDarwin
-          then "Library/Application Support/ArmCord/themes/OLED"
-          else ".config/ArmCord/themes/OLED";
+          then "Library/Application Support/armcord/themes/OLED"
+          else ".config/armcord/themes/OLED";
       in {
         "${target}/oled.theme.css".text = ''
           @import url(https://dimdengd.github.io/discord-oled-theme/code.css);
