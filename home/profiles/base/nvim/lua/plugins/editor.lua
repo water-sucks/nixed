5d968f82514ba957757b00b6a9fee0425d555015
Varun Narravula <varun@snare.dev>
2024-09-09 17:04:55 -0700

    fix(nvim): hardcode shiftwidth to 4

--- home/profiles/base/nvim/lua/plugins/editor.lua
+++ home/profiles/base/nvim/lua/plugins/editor.lua
@@ -25,6 +25,10 @@ local guess_indent_spec = use("NMAC427/guess-indent.nvim", {
         "NvimTree",
         "FTerm",
       },
+      on_tab_options = {
+        ["expandtab"] = false,
+        ["shiftwidth"] = 4,
+      },
     })
   end,
 })
