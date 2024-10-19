b36ea7fe3110060c5e2adb580a93f0bcf3b39641
Varun Narravula <varun@snare.dev>
2024-10-19 15:49:19 -0700

    fix: use stable version of nvfetcher

--- modules/shell.nix
+++ modules/shell.nix
@@ -26,7 +26,7 @@
           treefmt
           lefthook
           nodePackages.prettier
-          nvfetcher
+          stable.nvfetcher
         ];
         shellHook = ''
           ${(nixago.lib.${system}.makeAll nixagoConfigs).shellHook}
