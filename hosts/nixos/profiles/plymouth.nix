752c148499b39b1debe86c8a3d8136f29abac2ca
Varun Narravula <varun@snare.dev>
2024-10-19 22:33:12 -0700

    feat(plymouth): use nixos-blur theme

--- hosts/nixos/profiles/plymouth.nix
+++ hosts/nixos/profiles/plymouth.nix
@@ -12,11 +12,30 @@
       "boot.shell_on_fail"
     ];
 
-    plymouth = {
+    plymouth = let
+      plymouth_blur_theme = pkgs.stdenv.mkDerivation {
+        pname = "nixos-blur-theme";
+        version = "0.0.0";
+        src = pkgs.fetchgit {
+          url = "https://git.gurkan.in/gurkan/nixos-blur-plymouth";
+          rev = "ea75b51a1f04aa914647a2929eab6bbe595bcfc0";
+          hash = "sha256-BSmh+Gy3yJMA4RoJ0uaQ/WsYBs+Txr6K3cAQjf+yM5Y=";
+        };
+
+        dontConfigure = true;
+        dontBuild = true;
+
+        installPhase = ''
+          mkdir -p $out/share/plymouth/themes
+          cp -r nixos-blur $out/share/plymouth/themes
+        '';
+      };
+    in {
       enable = true;
       font = "${pkgs.ibm-plex}/share/fonts/opentype/IBMPlexSans-Text.otf";
-      theme = "deus_ex";
-      themePackages = [(pkgs.adi1090x-plymouth-themes.override {selected_themes = ["deus_ex"];})];
+      theme = "nixos-blur";
+      # themePackages = [(pkgs.adi1090x-plymouth-themes.override {selected_themes = ["deus_ex"];})];
+      themePackages = [plymouth_blur_theme];
     };
   };
 }
