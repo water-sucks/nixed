c78f70a685d6886e9ba88240dfc8c2f3f80e9d6f
Varun Narravula <varun@snare.dev>
2024-11-02 18:17:36 -0700

    fix(mail): isolate proton-bridge linux-only config

--- home/profiles/apps/proton-bridge.nix
+++ home/profiles/apps/proton-bridge.nix
@@ -3,40 +3,50 @@
   pkgs,
   lib,
   ...
-}: {
-  services.protonmail-bridge = {
-    enable = true;
-    package =
-      pkgs.runCommand "protonmail-bridge"
-      {
-        bridge = pkgs.protonmail-bridge;
-        nativeBuildInputs = [pkgs.makeWrapper];
-      }
-      ''
-        mkdir -p $out/bin
-        makeWrapper $bridge/bin/protonmail-bridge $out/bin/protonmail-bridge \
-            --set PATH ${lib.strings.makeBinPath [pkgs.gnome-keyring]}
-      '';
-  };
+}: let
+  inherit (pkgs.stdenv) isLinux;
+in
+  lib.mkMerge [
+    {
+      services.protonmail-bridge = {
+        enable = true;
+        package =
+          if isLinux
+          then
+            pkgs.runCommand "protonmail-bridge"
+            {
+              bridge = pkgs.protonmail-bridge;
+              nativeBuildInputs = [pkgs.makeWrapper];
+            }
+            ''
+              mkdir -p $out/bin
+              makeWrapper $bridge/bin/protonmail-bridge $out/bin/protonmail-bridge \
+                  --set PATH ${lib.strings.makeBinPath [pkgs.gnome-keyring]}
+            ''
+          else pkgs.protonmail-bridge;
+      };
+    }
 
-  systemd.user.services.gnome-keyring = {
-    Unit = {
-      PartOf = ["graphical-session.target"];
-    };
+    (lib.mkIf isLinux {
+      systemd.user.services.gnome-keyring = {
+        Unit = {
+          PartOf = ["graphical-session.target"];
+        };
 
-    Install = {WantedBy = ["graphical-session.target"];};
-  };
+        Install = {WantedBy = ["graphical-session.target"];};
+      };
 
-  services.gnome-keyring = {
-    enable = true;
-    components = ["pkcs11" "secrets" "ssh"];
-  };
+      services.gnome-keyring = {
+        enable = true;
+        components = ["pkcs11" "secrets" "ssh"];
+      };
 
-  home.persistence."${config.persistence.directory}" = {
-    directories = [
-      ".cache/protonmail"
-      ".config/protonmail"
-      ".local/share/protonmail"
-    ];
-  };
-}
+      home.persistence."${config.persistence.directory}" = {
+        directories = [
+          ".cache/protonmail"
+          ".config/protonmail"
+          ".local/share/protonmail"
+        ];
+      };
+    })
+  ]
