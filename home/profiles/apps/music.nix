3cccfd794ce6f5c4878887750ff3266ad4441e3b
Varun Narravula <varun@snare.dev>
2024-12-01 00:15:32 -0600

    fix(music): remove override for tidal-hifi package

--- home/profiles/apps/music.nix
+++ home/profiles/apps/music.nix
@@ -3,39 +3,26 @@
   pkgs,
   lib,
   ...
-}: let
-  tidal-hifi = pkgs.tidal-hifi.overrideAttrs (o: rec {
-    version = "5.13.1";
-    src = pkgs.fetchurl {
-      url = "https://github.com/Mastermindzh/tidal-hifi/releases/download/${version}/tidal-hifi_${version}_amd64.deb";
-      sha256 = "sha256-TnvVm0qjm41if90+f9YtO8wR6NYg6oUZbZd0RiUU33M=";
-    };
-    buildInputs =
-      o.buildInputs
-      ++ [
-        pkgs.libGL
-      ];
-  });
-in
-  lib.mkIf pkgs.stdenv.isLinux {
-    home.packages = [
-      tidal-hifi
+}:
+lib.mkIf pkgs.stdenv.isLinux {
+  home.packages = [
+    pkgs.tidal-hifi
+  ];
+
+  xdg.desktopEntries.tidal-hifi = {
+    name = "TIDAL Hi-Fi";
+    exec = "tidal-hifi --no-sandbox";
+    icon = "tidal-hifi";
+    terminal = false;
+  };
+
+  home.persistence.${config.persistence.directory} = {
+    directories = [
+      ".config/tidal-hifi"
     ];
+  };
 
-    xdg.desktopEntries.tidal-hifi = {
-      name = "TIDAL Hi-Fi";
-      exec = "${tidal-hifi}/bin/tidal-hifi --no-sandbox";
-      icon = "tidal-hifi";
-      terminal = false;
-    };
-
-    home.persistence.${config.persistence.directory} = {
-      directories = [
-        ".config/tidal-hifi"
-      ];
-    };
-
-    xdg.mimeApps.defaultApplications = {
-      "x-scheme-handler/tidal" = ["tidal-hifi.desktop"];
-    };
-  }
+  xdg.mimeApps.defaultApplications = {
+    "x-scheme-handler/tidal" = ["tidal-hifi.desktop"];
+  };
+}
