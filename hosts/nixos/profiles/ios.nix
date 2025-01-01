38f9e51da0941bf00a21b1bcb29eba4f0b16ef68
Varun Narravula <varun@snare.dev>
2024-12-31 19:39:53 -0800

    fix(ios): temporarily unbreak usbmuxd2 build

--- hosts/nixos/profiles/ios.nix
+++ hosts/nixos/profiles/ios.nix
@@ -6,6 +6,13 @@
 
   services.usbmuxd = {
     enable = true;
-    package = pkgs.usbmuxd2;
+    package = pkgs.usbmuxd2.overrideAttrs (o: {
+      postPatch =
+        o.postPatch
+        + ''
+          # Otherwise, it will complain about no matching function for call to 'find'
+          sed -i 1i'#include <algorithm>' usbmuxd2/Muxer.cpp
+        '';
+    });
   };
 }
