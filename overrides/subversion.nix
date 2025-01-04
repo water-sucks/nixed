15ca6db2e49b226768a4078579b76b493153b97b
Varun Narravula <varun@snare.dev>
2025-01-04 15:37:47 -0800

    revert: apply fixes to subversion package
    This reverts commit 96625960713185bde135cabbe214ca49f638507a.

--- overrides/subversion.nix
+++
@@ -1,9 +0,0 @@
-_final: prev: {
-  subversionClient = prev.subversionClient.overrideAttrs (_: {
-    # remove vendored swig-3 files as these will shadow the swig provided
-    # ones and result in compile errors
-    postPatch = ''
-      rm subversion/bindings/swig/proxy/{perlrun.swg,pyrun.swg,python.swg,rubydef.swg,rubyhead.swg,rubytracking.swg,runtime.swg,swigrun.swg}
-    '';
-  });
-}
