ee9f367da5a65cb24f9628e5f656e5627ccb80d2
Varun Narravula <varun@snare.dev>
2025-01-14 00:03:49 -0800

    refactor(nvim): use lua functions for gitsigns, disable printing

--- home/profiles/base/nvim/lua/plugins/git.lua
+++ home/profiles/base/nvim/lua/plugins/git.lua
@@ -18,24 +18,38 @@ diffview_spec.config = function()
 end
 
 gitsigns_spec.config = function()
+  local gitsigns = require("gitsigns")
   local wk = require("which-key")
 
-  require("gitsigns").setup({})
+  gitsigns.setup({})
 
   wk.add({
-    { "[c", "<cmd>Gitsigns next_hunk<CR>", desc = "Move to next hunk" },
-    { "]c", "<cmd>Gitsigns prev_hunk<CR>", desc = "Move to previous hunk" },
-
     { "<Leader>h", group = "Gitsigns" },
-    { "<Leader>hs", "<cmd>Gitsigns stage_hunk<CR>", desc = "Stage hunk", mode = { "n", "v" } },
-    { "<Leader>hu", "<cmd>Gitsigns undo_stage_hunk<CR>", desc = "Unstage hunk", mode = { "n", "v" } },
-    { "<Leader>hr", "<cmd>Gitsigns reset_hunk<CR>", desc = "Reset hunk", mode = { "n", "v" } },
-    { "<Leader>hp", "<cmd>Gitsigns preview_hunk<CR>", desc = "Preview hunk" },
-    { "<Leader>hS", "<cmd>Gitsigns stage_buffer<CR>", desc = "Stage buffer" },
-    { "<Leader>hR", "<cmd>Gitsigns reset_buffer_index<CR>", desc = "Unstage buffer" },
-    { "<Leader>hU", "<cmd>Gitsigns reset_buffer<CR>", desc = "Reset buffer" },
-    { "<Leader>hB", "<cmd>Gitsigns blame<CR>", desc = "Show blame window" },
-    { "<Leader>hb", "<cmd>Gitsigns toggle_current_line_blame<CR>", desc = "Toggle blame virtual text" },
+
+    {
+      "[c",
+      function()
+        gitsigns.nav_hunk("prev", { navigation_message = false })
+      end,
+      desc = "Move to next hunk",
+    },
+    {
+      "]c",
+      function()
+        gitsigns.nav_hunk("next", { navigation_message = false })
+      end,
+      desc = "Move to next hunk",
+    },
+
+    { "<Leader>hs", gitsigns.stage_hunk, desc = "Stage hunk", mode = { "n", "v" } },
+    { "<Leader>hu", gitsigns.undo_stage_hunk, desc = "Unstage hunk", mode = { "n", "v" } },
+    { "<Leader>hr", gitsigns.reset_hunk, desc = "Reset hunk", mode = { "n", "v" } },
+    { "<Leader>hp", gitsigns.preview_hunk, desc = "Preview hunk" },
+    { "<Leader>hS", gitsigns.stage_buffer, desc = "Stage buffer" },
+    { "<Leader>hR", gitsigns.reset_buffer_index, desc = "Unstage buffer" },
+    { "<Leader>hU", gitsigns.reset_buffer, desc = "Reset buffer" },
+    { "<Leader>hB", gitsigns.blame, desc = "Show blame window" },
+    { "<Leader>hb", gitsigns.toggle_current_line_blame, desc = "Toggle blame virtual text" },
 
     { "ih", "<C-U>Gitsigns select_hunk<CR>", desc = "Select hunk", mode = { "x", "o" } },
   })
