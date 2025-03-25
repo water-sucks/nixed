64534186e11089291f3d37ff812a26452875bb37
Varun Narravula <varun@snare.dev>
2025-03-25 15:20:55 -0700

    fix(legcord): remove broken custom theme
    Discord's new UI is trash. That is all I will say.
    
    But it at least has a builtin dark mode, so we are going to use that
    for now until themes support the new theme experiment.

--- home/profiles/apps/legcord.nix
+++ home/profiles/apps/legcord.nix
@@ -2,47 +2,15 @@
   pkgs,
   lib,
   ...
-}: let
-  inherit (pkgs.stdenv) isLinux isDarwin;
-in
-  lib.mkMerge [
-    (lib.mkIf isLinux {
-      home.packages = [
-        pkgs.legcord
-      ];
+}:
+lib.mkIf pkgs.stdenv.isLinux {
+  home.packages = [
+    pkgs.legcord
+  ];
 
-      persistence = {
-        directories = [
-          ".config/legcord"
-        ];
-      };
-    })
-    {
-      home.file = let
-        target =
-          if isDarwin
-          then "Library/Application Support/legcord/themes/OLED"
-          else ".config/legcord/themes/OLED";
-      in {
-        "${target}/oled.theme.css".text = ''
-          @import url(https://dimdengd.github.io/discord-oled-theme/code.css);
-
-          :is(:root, .theme-dark) {
-            --background-primary: black !important;
-            --background-secondary: black !important;
-            --background-tertiary: black !important;
-            --background-secondary-alt: black !important;
-            --background-accent: black !important;
-            --button-secondary-background: hsl(0 0% 10%) !important;
-            --activity-card-background: black;
-            --home-background: black !important;
-          }
-        '';
-        "${target}/manifest.json".text = builtins.toJSON {
-          name = "OLED";
-          author = "dimden";
-          theme = "oled.theme.css";
-        };
-      };
-    }
-  ]
+  persistence = {
+    directories = [
+      ".config/legcord"
+    ];
+  };
+}
