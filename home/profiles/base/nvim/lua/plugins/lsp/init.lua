2792f40e4b97115df0eeb4fc807008c465fbd58d
Varun Narravula <varun@snare.dev>
2025-03-30 19:52:01 -0700

    feat(nvim): remove keybinds that correspond to defaults
    I love 0.11

--- home/profiles/base/nvim/lua/plugins/lsp/init.lua
+++ home/profiles/base/nvim/lua/plugins/lsp/init.lua
@@ -10,10 +10,6 @@ local lspconfig_spec = use("neovim/nvim-lspconfig", {
             { "gD", vim.lsp.buf.declaration, desc = "Go to declaration" },
             { "gd", vim.lsp.buf.definition, desc = "Go to definition" },
             { "gh", vim.lsp.buf.hover, desc = "Hover docs" },
-            { "gi", vim.lsp.buf.implementation, desc = "Go to implementation" },
-            { "go", vim.lsp.buf.open_float, desc = "Show code actions" },
-            { "gr", vim.lsp.buf.rename, desc = "Rename" },
-            { "gx", vim.lsp.buf.code_action, desc = "Show code actions" },
           },
         })
       end,
