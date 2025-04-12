2b4b601bca30ee5e0db3f6678223424924d12fcd
Varun Narravula <varun@snare.dev>
2025-04-12 16:35:08 -0700

    feat(nvim): add command to toggle neocord

--- home/profiles/base/nvim/lua/plugins/fun.lua
+++ home/profiles/base/nvim/lua/plugins/fun.lua
@@ -72,7 +72,28 @@ local duck_spec = use("tamton-aquib/duck.nvim", {
 local neocord_spec = use("IogaMaster/neocord", {
   event = "VeryLazy",
   config = function()
-    require("neocord").setup({
+    local neocord = require("neocord")
+
+    vim.api.nvim_create_user_command("Neocord", function(command_opts)
+      local operation = command_opts.args == "" and "start" or command_opts.args
+
+      if operation == "start" then
+        neocord:connect()
+        vim.notify("started neocord", vim.log.levels.INFO)
+      elseif operation == "stop" then
+        neocord:stop()
+        vim.notify("stopped neocord", vim.log.levels.INFO)
+      else
+        vim.notify("Invalid argument. Use `:Neocord`, `:Neocord start` or `:Neocord stop`", vim.log.levels.ERROR)
+      end
+    end, {
+      nargs = "?",
+      complete = function(_, _, _)
+        return { "start", "stop" }
+      end,
+    })
+
+    neocord.setup({
       global_timer = true,
     })
   end,
