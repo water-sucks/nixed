9c3c18fea1c32bdb31a84766d9ae0f84d6f62178
Varun Narravula <varun@snare.dev>
2025-07-06 16:44:30 -0700

    fix(darwin): temporarily disable home-manager options on Darwin
    I need to generate these using some other means, possibly using
    a separate HM `mkOptionsList` function.

--- home/profiles/base/optnix.nix
+++ home/profiles/base/optnix.nix
@@ -8,59 +8,62 @@
   ...
 }: let
   optnixLib = inputs.optnix.mkLib pkgs;
+  inherit (pkgs.stdenv) isDarwin;
 in {
   programs.optnix = {
     enable = true;
     settings = {
       min_score = 3;
 
-      scopes = {
-        CharlesWoodson = {
-          description = "NixOS configuration for CharlesWoodson";
-          options-list-file = optnixLib.mkOptionsList {
-            inherit (self.nixosConfigurations.CharlesWoodson) options;
+      scopes =
+        {
+          CharlesWoodson = {
+            description = "NixOS configuration for CharlesWoodson";
+            options-list-file = optnixLib.mkOptionsList {
+              inherit (self.nixosConfigurations.CharlesWoodson) options;
+            };
+            evaluator = "nix eval $NIXOS_CONFIG#nixosConfigurations.CharlesWoodson.config.{{ .Option }}";
           };
-          evaluator = "nix eval $NIXOS_CONFIG#nixosConfigurations.CharlesWoodson.config.{{ .Option }}";
-        };
 
-        SebastianJanikowski = {
-          description = "NixOS configuration for SebastianJanikowski";
-          options-list-file = optnixLib.mkOptionsList {
-            inherit (self.nixosConfigurations.SebastianJanikowski) options;
+          SebastianJanikowski = {
+            description = "NixOS configuration for SebastianJanikowski";
+            options-list-file = optnixLib.mkOptionsList {
+              inherit (self.nixosConfigurations.SebastianJanikowski) options;
+            };
+            evaluator = "nix eval $NIXOS_CONFIG#nixosConfigurations.SebastianJanikowski.config.{{ .Option }}";
           };
-          evaluator = "nix eval $NIXOS_CONFIG#nixosConfigurations.SebastianJanikowski.config.{{ .Option }}";
-        };
 
-        TimBrown = {
-          description = "nix-darwin configuration for TimBrown";
-          options-list-file = optnixLib.mkOptionsList {
-            inherit (self.darwinConfigurations.TimBrown) options;
+          TimBrown = {
+            description = "nix-darwin configuration for TimBrown";
+            options-list-file = optnixLib.mkOptionsList {
+              inherit (self.darwinConfigurations.TimBrown) options;
+            };
+            evaluator = "nix eval $NIXOS_CONFIG#darwinConfigurations.TimBrown.config.{{ .Option }}";
           };
-          evaluator = "nix eval $NIXOS_CONFIG#darwinConfigurations.TimBrown.config.{{ .Option }}";
-        };
 
-        MarcusAllen = {
-          description = "nix-darwin configuration for MarcusAllen";
-          options-list-file = optnixLib.mkOptionsList {
-            inherit (self.darwinConfigurations.MarcusAllen) options;
+          MarcusAllen = {
+            description = "nix-darwin configuration for MarcusAllen";
+            options-list-file = optnixLib.mkOptionsList {
+              inherit (self.darwinConfigurations.MarcusAllen) options;
+            };
+            evaluator = "nix eval $NIXOS_CONFIG#darwinConfigurations.MarcusAllen.config.{{ .Option }}";
           };
-          evaluator = "nix eval $NIXOS_CONFIG#darwinConfigurations.MarcusAllen.config.{{ .Option }}";
-        };
-
-        home-manager = {
-          description = "home-manager configuration for all systems";
-          options-list-file = optnixLib.mkOptionsList {
-            inherit options;
-            transform = opt:
-              opt
-              // {
-                name = lib.removePrefix "home-manager.users.${config.home.username}." opt.name;
-              };
-            excluded = ["sops"];
+        }
+        // (lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
+          home-manager = {
+            description = "home-manager configuration for all systems";
+            options-list-file = optnixLib.mkOptionsList {
+              inherit options;
+              transform = opt:
+                opt
+                // {
+                  name = lib.removePrefix "home-manager.users.${config.home.username}." opt.name;
+                };
+              excluded = ["sops"];
+            };
+            evaluator = "";
           };
-          evaluator = "";
-        };
-      };
+        });
     };
   };
 }
