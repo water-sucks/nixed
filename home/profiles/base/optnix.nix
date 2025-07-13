120f4f43c2b155bff26479c74ef13163d8196d7b
Varun Narravula <varun@snare.dev>
2025-07-13 14:29:42 -0700

    fix(optnix): use hm docs generation function

--- home/profiles/base/optnix.nix
+++ home/profiles/base/optnix.nix
@@ -4,66 +4,62 @@
   options,
   config,
   pkgs,
-  lib,
   ...
 }: let
   optnixLib = inputs.optnix.mkLib pkgs;
-  inherit (pkgs.stdenv) isDarwin;
 in {
   programs.optnix = {
     enable = true;
     settings = {
       min_score = 3;
 
-      scopes =
-        {
-          CharlesWoodson = {
-            description = "NixOS configuration for CharlesWoodson";
-            options-list-file = optnixLib.mkOptionsList {
-              inherit (self.nixosConfigurations.CharlesWoodson) options;
-            };
-            evaluator = "nix eval $NIXOS_CONFIG#nixosConfigurations.CharlesWoodson.config.{{ .Option }}";
+      scopes = {
+        CharlesWoodson = {
+          description = "NixOS configuration for CharlesWoodson";
+          options-list-file = optnixLib.mkOptionsList {
+            inherit (self.nixosConfigurations.CharlesWoodson) options;
           };
+          evaluator = "nix eval $NIXOS_CONFIG#nixosConfigurations.CharlesWoodson.config.{{ .Option }}";
+        };
 
-          SebastianJanikowski = {
-            description = "NixOS configuration for SebastianJanikowski";
-            options-list-file = optnixLib.mkOptionsList {
-              inherit (self.nixosConfigurations.SebastianJanikowski) options;
-            };
-            evaluator = "nix eval $NIXOS_CONFIG#nixosConfigurations.SebastianJanikowski.config.{{ .Option }}";
+        SebastianJanikowski = {
+          description = "NixOS configuration for SebastianJanikowski";
+          options-list-file = optnixLib.mkOptionsList {
+            inherit (self.nixosConfigurations.SebastianJanikowski) options;
           };
+          evaluator = "nix eval $NIXOS_CONFIG#nixosConfigurations.SebastianJanikowski.config.{{ .Option }}";
+        };
 
-          TimBrown = {
-            description = "nix-darwin configuration for TimBrown";
-            options-list-file = optnixLib.mkOptionsList {
-              inherit (self.darwinConfigurations.TimBrown) options;
-            };
-            evaluator = "nix eval $NIXOS_CONFIG#darwinConfigurations.TimBrown.config.{{ .Option }}";
+        TimBrown = {
+          description = "nix-darwin configuration for TimBrown";
+          options-list-file = optnixLib.mkOptionsList {
+            inherit (self.darwinConfigurations.TimBrown) options;
           };
+          evaluator = "nix eval $NIXOS_CONFIG#darwinConfigurations.TimBrown.config.{{ .Option }}";
+        };
 
-          MarcusAllen = {
-            description = "nix-darwin configuration for MarcusAllen";
-            options-list-file = optnixLib.mkOptionsList {
-              inherit (self.darwinConfigurations.MarcusAllen) options;
-            };
-            evaluator = "nix eval $NIXOS_CONFIG#darwinConfigurations.MarcusAllen.config.{{ .Option }}";
+        MarcusAllen = {
+          description = "nix-darwin configuration for MarcusAllen";
+          options-list-file = optnixLib.mkOptionsList {
+            inherit (self.darwinConfigurations.MarcusAllen) options;
           };
-        }
-        // (lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
-          home-manager = {
-            description = "home-manager configuration for all systems";
-            options-list-file = optnixLib.mkOptionsList {
-              inherit options;
-              transform = opt:
-                opt
-                // {
-                  name = lib.removePrefix "home-manager.users.${config.home.username}." opt.name;
-                };
-              excluded = ["sops"];
-            };
-            evaluator = "";
+          evaluator = "nix eval $NIXOS_CONFIG#darwinConfigurations.MarcusAllen.config.{{ .Option }}";
+        };
+
+        home-manager = {
+          description = "home-manager configuration for all systems";
+          options-list-file = optnixLib.hm.mkOptionsListFromHMSource {
+            home-manager = inputs.home;
+            modules = [
+              inputs.sops-nix.homeManagerModules.sops
+              {
+                inherit (config) home;
+              }
+            ];
           };
-        });
+          evaluator = "";
+        };
+      };
     };
   };
 }
