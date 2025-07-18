5dd198cc718c17deed7b34869dbbf805cae63b27
Varun Narravula <varun@snare.dev>
2025-07-18 11:01:13 -0700

    fix(nvim): use vim.diagnostic.config() for defining LSP signs

--- home/profiles/base/nvim/lua/plugins/lsp/init.lua
+++ home/profiles/base/nvim/lua/plugins/lsp/init.lua
@@ -28,17 +28,16 @@ local lspconfig_spec = use("neovim/nvim-lspconfig", {
       update_in_insert = true,
     })
 
-    local signs = {
-      Error = "",
-      Warn = "",
-      Info = "",
-      Hint = "",
-    }
-
-    for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-    end
+    vim.diagnostic.config({
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = "",
+          [vim.diagnostic.severity.WARN] = "",
+          [vim.diagnostic.severity.INFO] = "",
+          [vim.diagnostic.severity.HINT] = "",
+        },
+      },
+    })
 
     require("plugins.lsp.servers")
   end,
