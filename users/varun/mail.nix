cf68cfdd300f6abdb2d22dc5f1d17a23c921e259
Varun Narravula <varun@snare.dev>
2025-12-13 22:09:34 -0800

    feat(mail): add pangolin work account

--- users/varun/mail.nix
+++ users/varun/mail.nix
@@ -16,6 +16,7 @@ in
   lib.mkMerge [
     {
       sops.secrets.email-password = sopsEmailPasswordBlock "password";
+      sops.secrets.work-email-password = sopsEmailPasswordBlock "work-password";
 
       accounts.email = {
         maildirBasePath = "Mail";
@@ -43,6 +44,75 @@ in
               create = "both";
               expunge = "both";
               remove = "both";
+            };
+          };
+
+          work = {
+            primary = false;
+            flavor = "gmail.com";
+
+            realName = "Varun Narravula";
+            address = "varun@pangolin.net";
+            userName = "varun@pangolin.net";
+            passwordCommand = "${cat} ${config.sops.secrets.work-email-password.path}";
+
+            aerc = {
+              enable = true;
+              extraAccounts = {
+                folders-sort = ["Inbox" "Starred" "Sent" "Drafts" "Archive" "Spam" "Trash"];
+              };
+            };
+
+            msmtp.enable = true;
+            mbsync = {
+              enable = true;
+              create = "both";
+              expunge = "both";
+              remove = "both";
+              groups.work.channels = let
+                channelConfigBlock = _: v:
+                  v
+                  // {
+                    extraConfig = {
+                      Create = "Near";
+                      CopyArrivalDate = "yes";
+                      MaxMessages = 1000000;
+                      MaxSize = "10m";
+                      Sync = "All";
+                      SyncState = "*";
+                    };
+                  };
+              in
+                lib.mapAttrs channelConfigBlock {
+                  inbox = {
+                    farPattern = "";
+                    nearPattern = "Inbox";
+                  };
+                  starred = {
+                    farPattern = "[Gmail]/Starred";
+                    nearPattern = "Starred";
+                  };
+                  sent = {
+                    farPattern = "[Gmail]/Sent Mail";
+                    nearPattern = "Sent";
+                  };
+                  drafts = {
+                    farPattern = "[Gmail]/Drafts";
+                    nearPattern = "Drafts";
+                  };
+                  archive = {
+                    farPattern = "[Gmail]/All Mail";
+                    nearPattern = "Archive";
+                  };
+                  spam = {
+                    farPattern = "[Gmail]/Spam";
+                    nearPattern = "Spam";
+                  };
+                  trash = {
+                    farPattern = "[Gmail]/Trash";
+                    nearPattern = "Trash";
+                  };
+                };
             };
           };
         };
