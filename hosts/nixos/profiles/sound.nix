eed5f06611bfb3a4602f04812461322d1890b985
Varun Narravula <varun@snare.dev>
2025-12-15 06:59:27 -0800

    feat(sound): disable wireplumber session suspend timeout

--- hosts/nixos/profiles/sound.nix
+++ hosts/nixos/profiles/sound.nix
@@ -8,5 +8,30 @@ _: {
     pulse.enable = true;
     jack.enable = true;
     socketActivation = true;
+
+    wireplumber = {
+      enable = true;
+      extraConfig = {
+        "disable-suspend" = {
+          "monitor.alsa.rules" = [
+            {
+              matches = [
+                {
+                  "device.name" = "~alsa_input.*";
+                }
+                {
+                  "device.name" = "~alsa_output.*";
+                }
+              ];
+              actions = {
+                update-props = {
+                  "session.suspend-timeout-seconds" = 0;
+                };
+              };
+            }
+          ];
+        };
+      };
+    };
   };
 }
