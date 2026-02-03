d56e424b296198aba0d736049a81c390b410d400
Varun Narravula <varun@snare.dev>
2026-02-02 16:45:25 -0800

    feat(power): enable tlp on SebastianJanikowski

--- hosts/nixos/machines/SebastianJanikowski/hardware-configuration.nix
+++ hosts/nixos/machines/SebastianJanikowski/hardware-configuration.nix
@@ -62,9 +62,20 @@
   systemd.services.systemd-udev-settle.enable = false;
   systemd.services.NetworkManager-wait-online.enable = false;
 
-  powerManagement = {
+  powerManagement.enable = true;
+
+  services.tlp = {
     enable = true;
-    powertop.enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      START_CHARGE_THRESH_BAT0 = 20;
+      STOP_CHARGE_THRESH_BAT0 = 20;
+
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MAX_PERF_ON_BAT = 50;
+    };
   };
 
   services.upower.enable = true;
