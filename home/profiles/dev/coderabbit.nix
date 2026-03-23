937fe4c8e202b5df3ff94bdcec2132bd0db756c7
Varun Narravula <varun@snare.dev>
2026-03-23 15:10:15 -0700

    fix(coderabbit): add to persistence

--- home/profiles/dev/coderabbit.nix
+++ home/profiles/dev/coderabbit.nix
@@ -1,13 +1,25 @@
 {
   inputs,
   pkgs,
+  lib,
   ...
 }: let
   inherit (pkgs.stdenv.hostPlatform) system;
+  inherit (pkgs.stdenv) isLinux;
 
   inherit (inputs.nix-ai-tools.packages.${system}) coderabbit-cli;
-in {
-  home.packages = [
-    coderabbit-cli
-  ];
-}
+in
+  lib.mkMerge [
+    {
+      home.packages = [
+        coderabbit-cli
+      ];
+    }
+    (lib.mkIf isLinux {
+      persistence = {
+        directories = [
+          ".coderabbit"
+        ];
+      };
+    })
+  ]
