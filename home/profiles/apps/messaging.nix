8562d4148c506e3c9f2f48af07b6da4f08667029
Varun Narravula <varun@snare.dev>
2026-05-30 21:51:45 -0700

    feat(messaging): make it a configurable module

--- home/profiles/apps/messaging.nix
+++ home/profiles/apps/messaging.nix
@@ -1,37 +1,54 @@
 {
+  config,
   pkgs,
   lib,
   ...
 }: let
   inherit (pkgs.stdenv) isLinux isDarwin;
-in
-  lib.mkMerge [
-    (lib.mkIf isLinux {
-      home.packages = with pkgs; [
-        element-desktop
-        signal-desktop
-        slack
-        zoom-us
-      ];
+
+  cfg = config.profiles.messaging;
+
+  inherit (lib) mkIf mkMerge optional optionals;
+  mkMessagingOption = name:
+    lib.mkOption {
+      type = lib.types.bool;
+      description = "${name} messaging application";
+      default = true;
+    };
+in {
+  options.profiles.messaging = {
+    slack.enable = mkMessagingOption "Slack";
+    element.enable = mkMessagingOption "Element";
+    zoom.enable = mkMessagingOption "Zoom";
+    signal.enable = mkMessagingOption "Signal";
+  };
+
+  config = mkMerge [
+    (mkIf isLinux {
+      home.packages =
+        optional cfg.slack.enable pkgs.slack
+        ++ optional cfg.element.enable pkgs.element-desktop
+        ++ optional cfg.zoom.enable pkgs.zoom-us
+        ++ optional cfg.signal.enable pkgs.signal-desktop;
 
       persistence = {
-        directories = [
-          ".config/Element"
-          ".config/Signal"
-          ".config/Slack"
-        ];
-        files = [
+        directories =
+          optional cfg.slack.enable ".config/Slack"
+          ++ optional cfg.element.enable ".config/Element"
+          ++ optional cfg.signal.enable ".config/Signal";
+
+        files = optionals cfg.zoom.enable [
           ".config/zoom.conf"
           ".config/zoomus.conf"
         ];
       };
     })
-    (lib.mkIf isDarwin {
-      homebrew.casks = [
-        "element"
-        "signal"
-        "slack"
-        "zoom"
-      ];
+    (mkIf isDarwin {
+      homebrew.casks =
+        optional cfg.slack.enable "slack"
+        ++ optional cfg.element.enable "element"
+        ++ optional cfg.zoom.enable "zoom"
+        ++ optional cfg.signal.enable "signal";
     })
-  ]
+  ];
+}
