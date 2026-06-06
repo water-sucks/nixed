8728a1ec97c9aee8fd97eef5fdb03a8837c3cce6
Varun Narravula <varun@snare.dev>
2026-06-06 01:10:03 -0700

    fix(bitwarden): when will we learn

--- home/profiles/apps/bitwarden.nix
+++ home/profiles/apps/bitwarden.nix
@@ -8,7 +8,10 @@ in
   lib.mkMerge [
     (lib.mkIf isLinux {
       home.packages = [
-        pkgs.bitwarden-desktop
+        # pkgs.bitwarden-desktop
+        # the desktop is broken again lol https://github.com/NixOS/nixpkgs/pull/525731
+        # when will we learn
+        pkgs.bitwarden-cli
       ];
 
       persistence = {
