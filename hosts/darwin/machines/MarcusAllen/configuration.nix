58cdf5d440fdd31478e6d0adb12947ff434bc144
Varun Narravula <varun@snare.dev>
2026-06-12 16:05:41 -0500

    feat(nix): add internal nix cache for infra

--- hosts/darwin/machines/MarcusAllen/configuration.nix
+++ hosts/darwin/machines/MarcusAllen/configuration.nix
@@ -5,6 +5,13 @@
 
   security.pam.services.sudo_local.touchIdAuth = true;
 
+  nix.settings = {
+    extra-substituters = ["https://artifact-s3-gateway.int.n7k.io/n7k-nix-cache"];
+    extra-trusted-public-keys = [
+      "nix-cache.infra.n7k.io-0:WyML6bRQeGqxs/1iSoQrlMuooDlxG15rgjuo7Elmpf4="
+    ];
+  };
+
   networking.hostName = "MarcusAllen";
 
   system.stateVersion = 6;
